bafc6a820e543ba31151246f9705b29bd575bd0e
gennadii <gennadiixd@gmail.com>
2019-10-19 13:43:58 +0300

    push,pop,shift ready

--- myArray.js
+++ myArray.js
@@ -1,30 +1,68 @@
-class myArray {
-    constructor(length = 0) {
-        this.length = length
-        this.data = {}
+const Assert = require('./test.js')
+
+class MyArray {
+    constructor(...args) {
+        this.length = 0
+        if (args) {
+            for (let i = 0; i < args.length; i++ , this.length++) {
+                this[i] = args[i]
+            }
+        }
+    }
+    reindex() {
+        const findSpace = () => {
+            for (let i = 0; i < this.length; i++) {
+                if (this[i] === undefined) {
+                    return i
+                }
+            }
+            return undefined
+        }
+
+        const findNearestValue = (index) => {
+            for (let i = index; i < this.length; i++) {
+                if (this[i] !== undefined) {
+                    return i
+                }
+            }
+            return undefined
+        }
+
+        let spaceIdx = findSpace();
+        while (spaceIdx !== undefined) {
+            let nearestValueIdx = findNearestValue(spaceIdx)
+            if (nearestValueIdx !== undefined) {
+                this[spaceIdx] = this[nearestValueIdx]
+                delete this[nearestValueIdx]
+            } else {
+                this.length = spaceIdx
+                delete this[spaceIdx]
+            }
+            spaceIdx = findSpace();
+        }
     }
 
-    push(item) {
-        this.data[this.length++] = item
+    push(...items) {
+        for (let i = 0; i < items.length; i++) {
+            this[this.length++] = items[i]
+        }
         return this.length
     }
 
     pop() {
+        if (this.length <= 0) return undefined;
         const number = this.length-- - 1
-        const toBeReturned = this.data[number];
-        const removeItem = (prop) => ({[prop]: _, ...rest}) => rest;
-        this.data = removeItem(number)(this.data);
+        const toBeReturned = this[number];
+        delete this[number];
         return toBeReturned;
+    }
+
+    shift() {
+        delete this[this.length-- - 1];
+        this.reindex()
     }
 }
 
-let myArr = new myArray();
-myArr.push(3)
-myArr.push(4)
-myArr.push(4)
+let assert = new Assert(MyArray);
 
-console.log(myArr.pop());
-myArr.push(3)
-console.log(myArr.pop());
-console.log(myArr.pop());
-
+assert.run();
---
+++ test.js
@@ -0,0 +1,62 @@
+module.exports = class Assert {
+    constructor(Arr){
+        this.Arr = Arr
+    }
+
+    run(){
+        this.assertCreate();
+        this.assertPush();
+        this.assertPop();
+    }
+
+    assertCreate(){
+        let arr = new this.Arr(1,2,3);
+        if (arr.length !==3){
+            throw new Error(`Length ${arr.length} shuld be 3`);
+        }
+        if (arr[2] !== 3) {
+            throw new Error(`Third element ${arr[2]} shuld be 3`);
+        }
+        console.log(`Test ===> creation <=== passed`);
+    }
+
+    assertPush(){
+        let arr = new this.Arr();
+        let lengthBefore = arr.length;
+        let returned = arr.push('test');
+        let lengthAfter = arr.length;
+        if (returned !== arr.length){
+            throw new Error(`returned length ${returned} not equal actual length ${arr.length}`);
+        }
+        if (lengthAfter - lengthBefore  !== 1){
+            throw new Error(`Length after = ${lengthAfter} Length before = ${lengthBefore}`);
+        }
+        if (arr[0] !== 'test'){
+            throw new Error(`Wrong pushed element value ${arr[0]} shuld be test`);
+        }
+        returned = arr.push('test2', 'test3', 'test4');
+        if (arr.length !== 4){
+            throw new Error(`Length ${arr.length} expected to be 4`);
+        }
+        console.log(`Test ===> push <=== passed`);
+    }
+
+    assertPop(){
+        let arr = new this.Arr();
+        let returned = arr.pop();
+        if (returned){
+            throw new Error(`should return undefined when popping empty array, returned ${returned}`)
+        }
+        arr.push('test');
+        returned = arr.pop();
+        let lengthBefore = arr.length;
+        let lengthAfter = arr.length;
+        if(returned !== 'test'){
+            throw new Error(`${returned} expected to be 'test'`);
+        }
+        if (lengthBefore - lengthAfter === 1) {
+            throw new Error(`Length after = ${lengthAfter} Length before = ${lengthBefore}`);
+        }
+        console.log(`Test ===> pop <=== passed`);
+    }
+}
